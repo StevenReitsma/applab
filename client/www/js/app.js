0d46c7fb8ecb38a7425578d596f72e2679c353b6
robbertvdg <robbert.gugten@student.ru.nl>
2015-05-21 13:19:52 +0200

    Design of friend list

--- client/www/js/app.js
+++ client/www/js/app.js
@@ -82,6 +82,14 @@ angular.module('starter', ['ionic', 'starter.config', 'starter.services', 'start
 		controller: 'RankingCtrl'
       }
     }
+  })
+    .state('app.friends', {
+    url: "/friends",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/friends.html"
+      }
+    }
   });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/');
